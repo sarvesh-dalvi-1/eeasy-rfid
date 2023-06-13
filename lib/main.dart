dde538fe0653f910df45b3c506b5ef53d642da98
charan9999 <38738068+charan9999@users.noreply.github.com>
2023-06-13 20:01:04 +0400

    updated repo

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,17 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
-  runApp(
-      MultiProvider(
-          providers: [
-            ChangeNotifierProvider<RfidInitProvider>(create: (_) => RfidInitProvider()),
-            ChangeNotifierProvider<RfidReadProvider>(create: (_) => RfidReadProvider()),
-            ChangeNotifierProvider<AppStateProvider>(create: (_) => AppStateProvider()),
-            ChangeNotifierProvider<CheckoutProvider>(create: (_) => CheckoutProvider()),
-          ],
-          child: const MyApp()
-      )
-  );
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<RfidInitProvider>(create: (_) => RfidInitProvider()),
+    ChangeNotifierProvider<RfidReadProvider>(create: (_) => RfidReadProvider()),
+    ChangeNotifierProvider<AppStateProvider>(create: (_) => AppStateProvider()),
+    ChangeNotifierProvider<CheckoutProvider>(create: (_) => CheckoutProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -33,15 +27,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eeasy rfid',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Wrapper()
-    );
+        title: 'eeasy rfid',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Wrapper());
   }
 }
-
-
-
